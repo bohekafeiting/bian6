f5d060ebeaae3fa7a37671684b65502f9d1b2797
bohekafeiting <1363896952@qq.com>
2016-06-04 20:17:31 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -2,21 +2,22 @@
 function fibonacci_series(n)
 {  array=new Array();
   var first=0,secend=1,sum=0;
-  if(n==0)
-    return 0;
+  if(n==1)
+  {array[0]=0;
+   array[1]=1;
+  }
   else	
   {
    array[0]=0;
    array[1]=1;
-  if(n>1)
-    for(var i=2;i<=n;i++)
+   for(var i=2;i<=n;i++)
 	{
 	  sum=first+secend;
 	  first=secend;
 	  array.push(sum);
 	  secend=array[i];
 	}
-   return array;
   }
+   return array;
 }
 module.exports = collect_same_elements;
